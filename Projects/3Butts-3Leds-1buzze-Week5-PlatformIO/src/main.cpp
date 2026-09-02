91db2f08f04ae42c5a3f79fd5eebae0eadbb2cfc
ShuaibKangan <200083238@student.kangan.edu.au>
2026-09-02 14:07:34 +1000

    .....

--- Projects/3Butts-3Leds-1buzze-Week5-PlatformIO/src/main.cpp
+++ Projects/3Butts-3Leds-1buzze-Week5-PlatformIO/src/main.cpp
@@ -5,7 +5,7 @@
 
 constexpr uint8_t ledPins[3] = {38, 39, 40};
 constexpr uint8_t buttons[3] = {42, 41, 1};
-constexpr uint8_t speaker[1] = {14};
+constexpr uint8_t speaker[1] = {11};
 
 
 
@@ -14,38 +14,36 @@ void setup() {
   Serial.begin(115200);
 
   for (int i = 0; i < 3; i++) {
-   pinMode(buttons[i], INPUT_PULLUP);   // configures all 4 pins with one loop, not four pinMode() calls
+   pinMode(buttons[i], INPUT_PULLUP);
+   pinMode(ledPins[i], OUTPUT);   // configures all 4 pins with one loop, not four pinMode() calls
   }
 
-  
-  for (int i = 0; i < 3; i++) {
-    pinMode(ledPins[i], OUTPUT);   // configures all 4 pins with one loop, not four pinMode() calls
-  }
 
-  for(int s = 0; s < 1; s++) {
-    pinMode(speaker[s], OUTPUT);
-  }
+    pinMode(speaker[0], OUTPUT);
+
 }
 
 void moogcity2() {
-  if(digitalRead(buttons[0]) == LOW) {
-    tone(speaker[0], NOTE_E4, 100);
-  delay(300);
-  tone(speaker[0], NOTE_G4, 100);
-  delay(300);
-  tone(speaker[0], NOTE_B4, 100);
-  delay(300);
-  tone(speaker[0], NOTE_D5, 100);
-  delay(300);
-  tone(speaker[0], NOTE_B4, 100);
-  delay(300);
-  tone(speaker[0], NOTE_G4, 100);
-  delay(300);
-  tone(speaker[0], NOTE_E4, 100);
-  delay(300);
-  tone(speaker[0], NOTE_G4, 100);
-  } else if (digitalRead(buttons[1]) == LOW){
+  if(digitalRead(buttons[0]) == LOW) { 
+  tone(speaker[0], 330);
+  delay(250);
+  tone(speaker[0], 392);
+  delay(250);
+  tone(speaker[0], 440);
+  delay(250);
+  tone(speaker[0], 494);
+  delay(250);
+  tone(speaker[0], 494);
+  delay(250);
+  tone(speaker[0], 587);
+  delay(250);
+  tone(speaker[0], 329);
+  delay(250);
+  tone(speaker[0], 392);
+  noTone(speaker[0]);
+  } else if(digitalRead(buttons[1]) == LOW) {
     noTone(speaker[0]);
+    delay(200);
   }
   
 }
@@ -63,7 +61,6 @@ void loop() {
   // put your main code here, to run repeatedly:
    lightSwitch();
    moogcity2();
-
 
 }
 
