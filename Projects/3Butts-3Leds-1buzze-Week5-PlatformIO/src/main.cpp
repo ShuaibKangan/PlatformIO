a6fc6084d9ef21caecf2a90c271c7af26ae2950f
ShuaibKangan <200083238@student.kangan.edu.au>
2026-09-02 15:16:23 +1000

    ....

--- Projects/3Butts-3Leds-1buzze-Week5-PlatformIO/src/main.cpp
+++ Projects/3Butts-3Leds-1buzze-Week5-PlatformIO/src/main.cpp
@@ -1,12 +1,12 @@
 #include <Arduino.h>
 #include <Wire.h>
-#include "pitches.h"
 // put function declarations here:
 
 constexpr uint8_t ledPins[3] = {38, 39, 40};
 constexpr uint8_t buttons[3] = {42, 41, 1};
 constexpr uint8_t speaker[1] = {11};
 
+uint16_t moogverse[7] = {30, 65, 86, 109, 120, 132, 150};
 
 
 void setup() {
@@ -24,28 +24,22 @@ void setup() {
 }
 
 void moogcity2() {
-  if(digitalRead(buttons[0]) == LOW) { 
-  tone(speaker[0], 330);
+  tone(speaker[0], 230);
   delay(250);
-  tone(speaker[0], 392);
+  tone(speaker[0], 390);
   delay(250);
   tone(speaker[0], 440);
   delay(250);
   tone(speaker[0], 494);
   delay(250);
-  tone(speaker[0], 494);
+  tone(speaker[0], 515);
   delay(250);
-  tone(speaker[0], 587);
+  tone(speaker[0], 574);
   delay(250);
-  tone(speaker[0], 329);
-  delay(250);
-  tone(speaker[0], 392);
-  noTone(speaker[0]);
-  } else if(digitalRead(buttons[1]) == LOW) {
-    noTone(speaker[0]);
-    delay(200);
-  }
-  
+  tone(speaker[0], 620);
+  delay(300);
+  tone(speaker[0], 690);
+  delay(350);
 }
 
 void lightSwitch() {
@@ -60,10 +54,26 @@ void lightSwitch() {
 void loop() {
   // put your main code here, to run repeatedly:
    lightSwitch();
-   moogcity2();
+
+  if (digitalRead(buttons[0]) == LOW) {
+  for (int d = 0; d < 6; d++) {
+    for (int i = 0; i < 7; i++) {
+      tone(speaker[0], moogverse[i] + (moogverse[i] * d));
+      if (i == 6) {
+        delay(350);
+      } else if (i == 7) {
+        delay(400);
+      } else {
+        delay(300);
+      }
+    }
+      
+  }
+  noTone(speaker[0]);
+  } else if (digitalRead(buttons[1]) == LOW) {
+    noTone(speaker[0]);
+    delay(200);
+  }
+
 
 }
-
-
-
-// put function definitions here:
