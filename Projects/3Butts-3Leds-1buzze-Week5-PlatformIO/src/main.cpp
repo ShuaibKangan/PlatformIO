59a965387a69d5aea27fcda863662fdd0b32c04d
ShuaibKangan <200083238@student.kangan.edu.au>
2026-09-02 11:28:36 +1000

    button masher 2000s

--- Projects/3Butts-3Leds-1buzze-Week5-PlatformIO/src/main.cpp
+++ Projects/3Butts-3Leds-1buzze-Week5-PlatformIO/src/main.cpp
@@ -1,35 +1,42 @@
 #include <Arduino.h>
-#include <wire.h>
+#include <Wire.h>
 // put function declarations here:
 
-constexpr uint8_t ledPin[3] = {33, 40, 18};
-constexpr uint8_t buttonz[3] = {37, 42, 41};
+constexpr uint8_t ledPins[3] = {38, 39, 40};
+constexpr uint8_t buttons[3] = {42, 41, 1};
 
 
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(ledPin[0, 1, 2], OUTPUT);
-  pinMode(buttonz[0, 1, 2], INPUT_PULLUP);
-  digitalWrite(ledPin[0, 1, 2], HIGH);
+
+  for (int i = 0; i < 3; i++) {
+   pinMode(buttons[i], INPUT_PULLUP);   // configures all 4 pins with one loop, not four pinMode() calls
+  }
+
+  
+  for (int i = 0; i < 3; i++) {
+    pinMode(ledPins[i], OUTPUT);   // configures all 4 pins with one loop, not four pinMode() calls
+  }
+}
 
 
+void lightSwitch() {
+  for (int i = 0; i <= 2; i++) {
+    if (digitalRead(buttons[i]) == LOW) {
+      digitalWrite(ledPins[i], HIGH);
+    } else {
+      digitalWrite(ledPins[i], LOW);    }
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+   lightSwitch();
 
 }
 
-void green(){
-  
-  if (digitalRead(buttonz[0]) == HIGH) {
-        digitalWrite(ledPin[0], LOW);
-  } else {
-    digitalWrite(ledPin[0], HIGH);
-  }
-}
 
 
 // put function definitions here:
